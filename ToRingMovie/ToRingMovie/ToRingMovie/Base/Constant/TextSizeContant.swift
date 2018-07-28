36d095d64942cc1f0880e5f76560e3832fc4d7d9
ToRing <nguyenthetrinh1996@gmail.com>
2018-07-28 22:54:01 +0700

    [create] create base

---
+++ ToRingMovie/ToRingMovie/ToRingMovie/Base/Constant/TextSizeContant.swift
@@ -0,0 +1,17 @@
+//
+//  TextSizeContant.swift
+//  ToRingMovieIOS
+//
+//  Created by TR on 7/26/18.
+//  Copyright © 2018 ToRing. All rights reserved.
+//
+
+import UIKit
+
+class TextSizeContant: NSObject {
+    let MINI = 10
+    let SMALL = 12
+    let NORMAL = 14
+    let LARGE = 16
+    let BIG = 18
+}
---
+++ ToRingMovie/ToRingMovie/ToRingMovie/Base/Extension/StringExtension.swift
@@ -0,0 +1,25 @@
+//
+//  AppString.swift
+//  ToRingMovie
+//
+//  Created by TR on 7/2/18.
+//  Copyright © 2018 ToRing. All rights reserved.
+//
+
+import UIKit
+
+extension String {
+    func height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let boundingBox = self.boundingRect(with: constraintRect,options: .usesLineFragmentOrigin, attributes: [NSAttributedStringKey.font: font], context: nil)
+        
+        return ceil(boundingBox.height)
+    }
+    
+    func width(withConstrainedHeight height: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedStringKey.font: font],context: nil)
+        return ceil(boundingBox.width)
+    }
+}
+
