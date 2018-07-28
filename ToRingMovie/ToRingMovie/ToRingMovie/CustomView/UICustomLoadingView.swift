7848220e89e3fc32efae270fc472a43900413a48
ToRing <nguyenthetrinh1996@gmail.com>
2018-07-28 22:56:30 +0700

    [create] create custom loading view

---
+++ ToRingMovie/ToRingMovie/ToRingMovie/CustomView/UICustomLoadingView.swift
@@ -0,0 +1,21 @@
+//
+//  UICustomLoadingView.swift
+//  ToRingMovieIOS
+//
+//  Created by TR on 7/23/18.
+//  Copyright © 2018 ToRing. All rights reserved.
+//
+
+import UIKit
+
+class UICustomLoadingView: UIActivityIndicatorView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
