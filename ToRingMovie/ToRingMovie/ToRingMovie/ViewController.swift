1379ab53755c07abf21975062b2a09713f1b01e4
ToRing <nguyenthetrinh1996@gmail.com>
2018-07-28 22:41:06 +0700

    [create] create project

---
+++ ToRingMovie/ToRingMovie/ToRingMovie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToRingMovie
+//
+//  Created by TR on 7/28/18.
+//  Copyright © 2018 ToRing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
